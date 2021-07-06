35df79890a43a3a865bac45beb96ab41c12efc23
ichsanachmad <ichsanachmad33@gmail.com>
2021-07-06 17:38:38 +0700

    add env

---
+++ lib/env/env.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnvifyGenerator
+// **************************************************************************
+
+class _Env {
+  static const String apiUrl = 'https://api.themoviedb.org/3/';
+  static const String apiKey = '73df602186fec3d2c2eb7878df8338b5';
+}
