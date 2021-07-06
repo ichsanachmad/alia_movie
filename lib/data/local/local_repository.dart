758176e59513877645f5c6690d2e0aaa2be4cc11
ichsanachmad <ichsanachmad33@gmail.com>
2021-07-07 02:13:46 +0700

    add entity

---
+++ lib/data/local/local_repository.dart
@@ -0,0 +1,40 @@
+import 'package:alia_movie/data/local/entity/movie_schedule.dart';
+import 'package:moor_flutter/moor_flutter.dart';
+
+part 'local_repository.g.dart';
+
+@UseMoor(tables: [MovieSchedule])
+class LocalRepository extends _$LocalRepository {
+  static const DB_PATH = "movie_schedule.sqlite";
+
+  LocalRepository()
+      : super(
+          FlutterQueryExecutor.inDatabaseFolder(
+              path: DB_PATH, logStatements: true),
+        );
+
+  @override
+  int get schemaVersion => 1;
+
+  Stream<List<MovieScheduleData>> getMovieScheduleStream() =>
+      select(movieSchedule).watch();
+
+  Future insertMovieSchedule(MovieScheduleCompanion movieScheduleData) =>
+      into(movieSchedule).insert(movieScheduleData);
+
+  Future updateMovieSchedule(MovieScheduleCompanion movieScheduleData) =>
+      (update(movieSchedule)
+            ..where((tbl) => (tbl.id.equals(movieScheduleData.id.value))))
+          .write(movieScheduleData);
+
+  Future<void> deleteMovieSchedule(
+          MovieScheduleCompanion movieScheduleData) async =>
+      await (delete(movieSchedule)
+            ..where((tbl) => (tbl.id.equals(movieScheduleData.id.value))))
+          .go();
+}
+
+LocalRepository? _instance;
+LocalRepository get getLocalRepositoryInstance {
+  return _instance ??= LocalRepository();
+}
