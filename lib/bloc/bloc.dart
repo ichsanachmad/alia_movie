4eb5df36ee22b054b4c260b32445672b372284d7
ichsanachmad <ichsanachmad33@gmail.com>
2021-07-07 21:02:27 +0700

    add get movie schedule detail bloc

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -13,3 +13,11 @@ export 'add_movie_schedule/add_movie_schedule_state.dart';
 export 'delete_movie_schedule/delete_movie_schedule_bloc.dart';
 export 'delete_movie_schedule/delete_movie_schedule_event.dart';
 export 'delete_movie_schedule/delete_movie_schedule_state.dart';
+
+export 'update_movie_schedule/update_movie_schedule_bloc.dart';
+export 'update_movie_schedule/update_movie_schedule_event.dart';
+export 'update_movie_schedule/update_movie_schedule_state.dart';
+
+export 'get_movie_schedule_detail/get_movie_schedule_detail_bloc.dart';
+export 'get_movie_schedule_detail/get_movie_schedule_detail_event.dart';
+export 'get_movie_schedule_detail/get_movie_schedule_detail_state.dart';
