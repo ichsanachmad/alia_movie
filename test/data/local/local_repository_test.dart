0e653421a2a633968796ab325c1796a7dbcab000
ichsanachmad <ichsanachmad33@gmail.com>
2021-07-08 17:45:57 +0700

    add test

---
+++ test/data/local/local_repository_test.dart
@@ -0,0 +1,5 @@
+import 'package:alia_movie/data/local/local_repository.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+// Moor Flutter not Supported for Test yet.
